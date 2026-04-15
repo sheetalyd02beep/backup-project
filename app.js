1e68afefa78ee5d307bf6225ce63144879dde707
Sheetal <sheetalyd02@gmail.com>
2026-04-16 01:48:09 +0530

    fix for cloud path

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  fs.writeFileSync('/data/file.txt', 'Backup + Cloud working!');
+  fs.writeFileSync('file.txt', 'Backup + Cloud working!');
+  
   res.send('App running with Docker + Cloud!');
 });
 
